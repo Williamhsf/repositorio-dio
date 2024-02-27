9a64810190e21acf7120fadad065689687aae0b7
Williamhsf <williamhenriquefb@gmail.com>
2024-02-27 13:37:24 -0300

    update files

--- comecando_na_biblioteca_React/dio-clone/src/pages/login/index.jsx
+++ comecando_na_biblioteca_React/dio-clone/src/pages/login/index.jsx
@@ -1,10 +1,50 @@
-import { Link } from 'react-router-dom'
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import {
+  Container,
+  Title,
+  Column,
+  TitleLogin,
+  SubtitleLogin,
+  EsqueciText,
+  CriarText,
+  Row,
+  Wrapper,
+} from "./styles";
 
 const Login = () => {
-  return (<>
-  <h1>Login</h1>
-  <Link to="/">Voltar para home</Link>
-  </>)
-}
+  return (
+    <>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+        <Column>
+          {/* vai envolver todo o formulário */}
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+            <form>
+              <Input placeholder="E-mail" />
+              <Input placeholder="Senha" type="password" />
 
-export { Login }
+              <Button title="Entrar" variant="secondary" type="submit" />
+            </form>
+            <Row>
+              <EsqueciText>Esqueci minha senha</EsqueciText>
+              <CriarText>Criar Conta</CriarText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
+    </>
+  );
+};
+
+export { Login };
